69b7408af8e64b218b4289f223122ea8ad1a2513
Haley <hne3@case.edu>
2015-04-05 13:57:59 -0400

    Retrieves one element from user and stores in t0

--- binarysearch.asm
+++ binarysearch.asm
@@ -2,7 +2,7 @@
 getN:
 .asciiz "\Please insert the number of integers in your list.\n"
 getList:
-.asciiz "\Please enter your sorted list.\n"
+.asciiz "\Please enter your sorted list one number at a time.\n"
 getSearchInput:
 .asciiz "What value would you like to search for?\n"
 confirmation:
@@ -15,3 +15,14 @@ li $v0, 4
 syscall
 li $v0, 5
 syscall # Receives number of integers
+add $t0, $v0, $zero
+la $a0, getList
+li $v0, 4
+syscall # Asks for elements in array, one at a time.
+add $s0, $zero, $zero # Initializes counter for ArrayLoop.
+ArrayLoop:
+li $v0, 5
+syscall
+add $t1, $v0, $zero # Adds input number to array. TO EDIT: needs to store each integer in a different location.
+addi $s0, $s0, 1 # Increments counter
+bne $s0, $t0, ArrayLoop # Loops until finished
